118f97de643391195a9ae65322f8311a15eaa8ee
Yue Lyu <lyu.yue1992@gmail.com>
2017-03-28 22:25:59 -0400

    debug

--- main.c
+++ main.c
@@ -118,7 +118,7 @@ int leader_of_installed() {
 int shift_to_leader_election(int view_id) {
     printf("shift_to_leader_election %d\n", view_id);
     // clear vc_entry set
-    if (last_attempted > view_id) bzero(&vc_entry[0], MAX_HOST);
+    if (last_attempted < view_id) bzero(&vc_entry[0], MAX_HOST);
 
     last_attempted = view_id;
 
@@ -277,7 +277,7 @@ int main(int argc, char* argv[]) {
         // if progress_timer expired, shift to leader election
         time(&cur_time);
         if (cur_time - progress_timer > progress_threshold) {
-            if (last_attempted < last_installed || 
+            if (last_attempted <= last_installed || 
                     (last_attempted > last_installed && last_attempted % host_n != self_id)) {
                 last_attempted = (last_attempted / host_n + 1) * host_n + self_id;
             }
