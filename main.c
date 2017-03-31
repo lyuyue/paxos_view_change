02fa02ddb02d75e47cef310e6d82c0fe99efbe0c
Yue Lyu <lyu.yue1992@gmail.com>
2017-03-31 14:27:37 -0400

    debug

--- main.c
+++ main.c
@@ -158,6 +158,7 @@ int preinstall_ready() {
 int jump_to_new_view(uint32_t view_id) {
     bzero(&vc_entry[0], MAX_HOST);
     last_installed = view_id;
+    progress_threshold = init_threshold;
     printf("%d: Server %d is the new leader of view %d\n", 
         self_id, last_installed % host_n, last_installed);
     time(&progress_timer);
